6d88d668b1aec10c5949ef5d04d0d53b19a1766b
jiyumi <jiyumi00@naver.com>
2023-10-21 22:47:50 +0900

    #10808

--- 문자열, 누적합, 구현/10808.cpp
+++ 문자열, 누적합, 구현/10808.cpp
@@ -4,16 +4,15 @@ using namespace std;
 int main(){
 	char alphabet[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
 	'p','q','r','s','t','u','v','w','x','y','z'};
-	char input[100];
+	char input[101];
+	//string input -> input.size()
 	int cnt=0;
 	vector<char>alphabetCount;
 	
-	
 	cin>>input;
-	cout<<sizeof(input)<<'\n';
 	for(int i=0;i<26;i++){
 		cnt=0;
-		for(int j=0;j<100;j++){
+		for(int j=0;input[j]!='\0';j++){
 			if(alphabet[i]==input[j]){
 				cnt++;
 			}
