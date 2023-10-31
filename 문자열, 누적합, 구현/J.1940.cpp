edfd46ea0cadc5380d6ef9e7f2649eee2629ff43
jiyumi <jiyumi00@naver.com>
2023-10-31 21:25:13 +0900

    add 1629

--- 문자열, 누적합, 구현/J.1940.cpp
+++ 문자열, 누적합, 구현/J.1940.cpp
@@ -14,8 +14,8 @@ int main(){
 	}
 	
 	for(int i=0;i<materialCnt;i++){
-		for(int j=i+1;j<materialCnt;j++){
-			if(materialNumber[i]+materialNumber[j]==sum){
+		for(int j=i;j<materialCnt;j++){
+			if(materialNumber[i]+materialNumber==sum){
 				cnt++;
 			}
 		}
