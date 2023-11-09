37f02a2983827cf13834e8a9a452ff2b685e1f61
jiyumi <jiyumi00@naver.com>
2023-11-09 14:17:09 +0900

    2583

--- 그래프, DFS, BFS/J.2583.cpp
+++ 그래프, DFS, BFS/J.2583.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 
 int a[104][104],visited[104][104];
-int m,n,k,lx,ly,rx,ry,ret;
-int dx[4]={-1,0,1,0};
-int dy[4]={0,1,0,-1};
+int m,n,k,lx,ly,rx,ry,cnt,ret;
+int dy[4]={-1,0,1,0};
+int dx[4]={0,-1,0,1};
+vector<int> cnts;
 
 void dfs(int x,int y){
-	cout<<"("<<x<<", "<<y<<")"<<'\n';
 	visited[x][y]=1;
 	for(int i=0;i<4;i++){
 		int nx=x+dx[i];
 		int ny=y+dy[i];
-		if(ny<0 || nx<0 || ny>=m || nx>=n) continue;
+		if(ny<1 || nx<1 || ny>m || nx>n) continue;
 		if(visited[nx][ny]==0 && a[nx][ny]==1){
+			cnt++;
 			dfs(nx,ny);
 		}
 	}
@@ -27,9 +28,7 @@ int main(){
 		}
 	}
 	for(int v=0;v<k;v++){
-
 		cin>>lx>>ly>>rx>>ry;
-		
 		for(int i=1;i<=n;i++){
 			for(int j=1;j<=m;j++){
 				if(i>=lx+1 && i <=rx && j>=ly+1 && j<=ry){
@@ -42,12 +41,16 @@ int main(){
 	for(int i=1;i<=n;i++){
 		for(int j=1;j<=m;j++){
 			if(a[i][j]==1 && visited[i][j]==0){
-				cout<<i<<", "<<j<<'\n';
 				dfs(i,j);
-			
+				cnt++;
+				cnts.push_back(cnt);
+				cnt=0; ret++;
 			}
 		}
-		
 	}
-	
+	sort(cnts.begin(),cnts.end());
+	cout<<ret<<'\n';
+	for(int item: cnts){
+		cout<<item<<' ';
+	}
 }
