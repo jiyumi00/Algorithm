ba8d89e9de2445fe2b1ccd3ebddcb38e065a27d2
jiyumi <jiyumi00@naver.com>
2023-11-23 19:57:25 +0900

    add 2828

--- Beakjoon/그래프, DFS, BFS/J.2828.cpp
+++ Beakjoon/그래프, DFS, BFS/J.2828.cpp
@@ -1,13 +1,30 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 
-int n,m,appleCnt,seq;
+int ret;
+int n,m,j,index,lastIndex=1;
 int main(){
 	cin>>n>>m;
-	cin>>appleCnt;
+	cin>>j;
 	
-	for(int i=0;i<appleCnt;i++){
-		cin>>seq
-		
+	for(int i=0;i<j;i++){
+		cin>>index;
+		if(lastIndex<index){
+			for(int j=lastIndex;j<index-(m-1);j++){
+				ret++;
+			}
+			lastIndex=index-(m-1);
+		}
+		else if(lastIndex>index){
+			for(int j=index;j<lastIndex;j++){
+				ret++;
+			}
+			lastIndex=index;
+		}
+		else{
+			continue;
+		}
 	}
+	cout<<ret<<'\n';
+	
 }
